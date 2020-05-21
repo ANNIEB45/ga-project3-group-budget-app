8d6bfdd99ed31f910438960ea37fcba41997d9f5
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-05-21 15:45:40 -0400

    added nav bar and styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import axios from 'axios'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+
+import './App.css'
 
 export default class App extends React.Component{
   render() {
     return (
-      <div>
-        <h1>Weekender Budget App</h1>
-        <Router>
+
+      <Router>
+        <nav className="nav-bar">
+          <Link to='/'>Home</Link>
+          <Link>Trips</Link>
+          <Link>Expenses</Link>
+        </nav>
           <Switch>
             <Route>
-              
+
             </Route>
           </Switch>
         </Router>
-      </div>
       
     )
   }
