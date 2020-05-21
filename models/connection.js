63167f7a2852af24dd3ceab5654247f93f405322
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-05-21 13:32:35 -0400

    updated

--- models/connection.js
+++ models/connection.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose')
 
+const connectionString = process.env.MONGODB_URI || 'mongodb://localhost/budget-app'
+
+mongoose.connect(connectionString)
+    .then(() => {
+        console.log('connected to mongo at: ', connectionString)
+        console.log('connected to database successfully 😊😊😊')
+    })
+    .catch(err => {
+        console.log('failed to connect to database 😱😱😱')
+        console.log(err)
+    })
