3572df7077a9fa94f864b41f82e4b61b6dc35a9b
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-05-22 12:07:39 -0400

    created categories component for expense components

---
+++ client/src/components/Expenses-Components/Categories.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Categories extends Component {
+    render() {
+        return (
+            <div>
+                <span class="material-icons">
+                    flight
+            </span>
+                
+            </div>
+        )
+    }
+}
