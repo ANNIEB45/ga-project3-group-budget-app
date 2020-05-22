0c4003fd2e7f1ee67732d9870a9d5b32a2852def
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-05-22 12:08:22 -0400

    modified expense form component

--- client/src/components/Expenses-Components/expensesForm.js
+++ client/src/components/Expenses-Components/expensesForm.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
+import Icons from './Categories'
+
+import './Expenses.css'
 
 export default class expensesForm extends Component {
-   
+
     state = {
-        name: '',
-        category: '',
+        note: '',
+        category: [
+            'Hotel',
+            'Flight',
+            'Entertainment',
+            'Food',
+            'Transportation',
+            'Ticket',
+            'Fees & Charges',
+
+        ],
         amount: '',
         savedAmt: ''
     }
@@ -12,7 +24,86 @@ export default class expensesForm extends Component {
     render() {
         return (
             <div>
-                
+                <form onSubmit={ this.handleOnSubmit }>
+                    
+                    {/* <div className="categories">
+                        <label>Choose a Category</label>
+                        <label>Hotel</label>
+                        <input
+                            type="checkbox"
+                            name="category"
+                            value={ this.state.category[0] }
+                            onChange={ this.handleOnChange }
+                        />
+                        <label>Flight</label>
+                        <input
+                            type="checkbox"
+                            name="category"
+                            value={ this.state.category[1] }
+                            onChange={ this.handleOnChange }
+                        />
+                        <label>Entertainment</label>
+                        <input
+                            type="checkbox"
+                            name="category"
+                            value={ this.state.category[2] }
+                            onChange={ this.handleOnChange }
+                        />
+                        <label>Food</label>
+                        <input
+                            type="checkbox"
+                            name="category"
+                            value={ this.state.category[3] }
+                            onChange={ this.handleOnChange }
+                        />
+                        <label>Transportation</label>
+                        <input
+                            type="checkbox"
+                            name="category"
+                            value={ this.state.category[4] }
+                            onChange={ this.handleOnChange }
+                        />
+                        <label>Ticket</label>
+                        <input
+                            type="checkbox"
+                            name="category"
+                            value={ this.state.category[5] }
+                            onChange={ this.handleOnChange }
+                        />
+                        <label>Fees and Charges</label>
+                        <input
+                            type="checkbox"
+                            name="category"
+                            value={ this.state.category[6] }
+                            onChange={ this.handleOnChange }
+                        />
+                    </div> */}
+
+                    <Icons />
+
+                    <label>Amount</label>
+                    <input
+                        type="text"
+                        name="amount"
+                        value={ this.state.amount }
+                        onChange={ this.handleOnChange } />
+                    <label>Amount Saved</label>
+                    <input
+                        type="text"
+                        name="savedAmt"
+                        value={ this.state.savedAmt }
+                        onChange={ this.handleOnChange } />
+                    <label>Notes</label>
+                    <input
+                        type="text"
+                        name="note"
+                        value={ this.state.note }
+                        onChange={ this.handleOnChange } />
+
+                    <input type="submit" value="Add Expense" />
+
+                </form>
+
             </div>
         )
     }
