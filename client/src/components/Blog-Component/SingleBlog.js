6a7499ba25c4854d8b5178413caea47162d52d96
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-05-25 17:22:17 -0400

    singleBlog component renders as expected

--- client/src/components/Blog-Component/SingleBlog.js
+++ client/src/components/Blog-Component/SingleBlog.js
@@ -5,19 +5,39 @@ import { Link } from 'react-router-dom'
 export default class SingleBlog extends Component{
 
     state = {
-        newBlog: {
             title: '',
             author: '',
-            date: Date,
+            date: '',
             post: '',
             image: ''
+        
+    }
+
+    componentDidMount() {
+        this.getBlogById()
+    }
+
+    getBlogById = async () => {
+        try {
+            const blogId = this.props.match.params.blogId
+            console.log('blog Id', blogId)
+            const res = await axios.get(`/api/blog/${blogId}`)
+            this.setState(res.data)
+        } catch (err) {
+            console.log('failed to get single blog')
+            console.log(err)
         }
     }
-    
     render() {
         return (
             <div>
-
+                <div>{ this.state.title }</div>
+                <div>Author: { this.state.author }</div>
+                <div>{ this.state.date }</div>
+                <div>
+                    <img src={this.state.image} />
+                </div>
+                <div>{ this.state.post }</div>
             </div>
         )
     }
