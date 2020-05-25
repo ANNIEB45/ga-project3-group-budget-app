d6a151b15d19f2cdd16f5fbddb6041dce9baeec9
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-05-25 17:02:30 -0400

    updated image input field

--- client/src/components/Blog-Component/NewBlogForm.js
+++ client/src/components/Blog-Component/NewBlogForm.js
@@ -19,56 +19,64 @@ export default class NewBlogForm extends Component {
         console.log(evt.target.value)
     }
 
-    handleSubmit = (evt) => {
+    handleSubmit = async (evt) => {
         evt.preventDefault()
-        
-          
+        console.log('i was clicked')
+        try {
+            await axios.post('/api/blog', this.state.newBlog)
+            this.props.getAllBlogs()
+        } catch (err) {
+            console.log('failed to create blog')
+            console.log(err)
+        }
     }
 
     render() {
         return (
             <div>
-                <form onSubmit={this.handleSubmit}>
-                <input 
+
+                <form onSubmit={ this.handleSubmit }>
+                    
+                    <input
                         type="text"
                         name="image"
                         placeholder="Image URL"
                         value={ this.state.newBlog.image }
                         onChange={ this.handleOnchange } />
-                    
-                    <input 
+
+                    <input
                         type="text"
                         name="title"
                         placeholder="Title of Post"
                         value={ this.state.newBlog.title }
                         onChange={ this.handleOnchange } />
-                    
-                    <input 
+
+                    <input
                         type="text"
                         name="author"
                         placeholder="Author"
                         value={ this.state.newBlog.author }
                         onChange={ this.handleOnchange } />
-                    
-                    <input 
+
+                    <input
                         type="date"
                         name="date"
                         value={ this.state.newBlog.date }
                         onChange={ this.handleOnchange } />
-                    
-                    <input 
+
+                    <input
                         type="text"
                         name="post"
                         placeholder="Write your post here"
                         value={ this.state.newBlog.post }
                         onChange={ this.handleOnchange } />
-                    
+
                     <input
                         type="submit"
                         value="Create Post" />
-                    
+
                 </form>
-                
+
             </div>
         )
     }
