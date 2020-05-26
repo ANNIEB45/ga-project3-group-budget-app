c60dbec2ca88bff36f529c12faf22b5637388c5e
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-05-25 23:25:35 -0400

    added styling to all blog page

--- client/src/components/Blog-Component/BlogMainPage.js
+++ client/src/components/Blog-Component/BlogMainPage.js
@@ -4,6 +4,9 @@ import { Link } from 'react-router-dom'
 
 //Components
 import BlogForm from './NewBlogForm'
+import SingleBlog from './SingleBlog'
+
+import './Blog.css'
 
 export default class BlogMainPage extends React.Component {
 
@@ -41,39 +44,56 @@ export default class BlogMainPage extends React.Component {
 
     render() {
         return (
-            <div>
-                <div>Weekender Blog</div>
-                {/* hero-type banner w/title and h3-heading w/caption */ }
+            <div className='main'>
+                <div className='blogpage-container'>
+                    <div className='blog-banner'>
+                        <h1>Weekender Blog</h1>
+                        <h3>Worry less about your wallet</h3>
 
-                {/* Links to different section of blog page:
+                    </div>
+                    {/* hero-type banner w/title and h3-heading w/caption */ }
+
+                    {/* Links to different section of blog page:
                 Link:homepage, article page, resources page */}
-                <div>
-                    <Link to='/blog'>Home</Link>
-                    <Link to='/blog/article'>Article</Link>
-                    <Link to='/blog/resources'>Resources</Link>
-                    <Link to='#'>LinkedIn</Link>
-                    <Link to='#'>Twitter</Link>
-                    <Link to='#'>Github</Link>
+                    <div className='link-banner'>
+                        <Link to='/blog'>Home</Link>
+                        <Link to='#'>Article</Link>
+                        <Link to='#'>Resources</Link>
+                        <div className="social-links">
+                            <Link to='#'>
+                                <img src="linkedin-nofill.png" />
+                            </Link>
+                            <Link to='#'>
+                                <img src="twitter-nofill.png" />
+                            </Link>
+                            <Link to='#'>
+                                <img src="github-nofill.png" />
+                            </Link>
+                        </div>
 
+                    </div>
                 </div>
                 <BlogForm
                     getAllBlogs={ this.getAllBlogs }/>
 
 
-                    { this.state.allBlogs.map((blog) => {
-                        return (
-                            <div>
+                { this.state.allBlogs.map((blog) => {
+                    return (
+                        <div className='content-container'>
+                            <div className='content-wrapper'>
                                 <img src={ blog.image } />
-                                <Link to={`/blog/${blog._id}`}>
-                                <div>{ blog.title }</div>
-                                    <div>{ blog.author }</div>
+                                <div className='content'>
+                                    <Link to={ `/blog/${blog._id}` }>
+                                        <div className='title'>{ blog.title }</div>
+                                        <div className='author'>{ blog.author }</div>
                                     </Link>
-                                <div>{ blog.date }</div>
-                                <div>{ blog.post }</div>
-                                <button onClick={ () => this.onDeleteBlog(blog._id) }>Delete</button>
+                                    <div>{ blog.post }</div>
+                                    <button onClick={() => this.onDeleteBlog}>Delete</button>
+                                </div>
                             </div>
-                        )
-                    }) }
+                        </div>
+                    )
+                }) }
 
             </div>
 
