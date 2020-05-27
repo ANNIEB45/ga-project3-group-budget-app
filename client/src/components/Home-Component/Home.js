fad34ed294f53be5c321ad773136718717386d3e
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-05-26 20:28:39 -0400

    updated component

--- client/src/components/Home-Component/Home.js
+++ client/src/components/Home-Component/Home.js
@@ -3,10 +3,7 @@ import axios from 'axios'
 import { Link } from 'react-router-dom'
 
 import CreateForm from './CreateEventFrom'
-
-import ExpenseForm from '../Expenses-Components/ExpensesForm'
-
-
+import SingleEvent from './SingleOuting'
 
 import './Home.css'
 
@@ -15,15 +12,12 @@ export default class Home extends Component {
 
     state = {
         allEvents: [],
-        allExpenses: [],
         showAddEventForm: false,
         showExpenseField: false,
-
     }
 
     componentDidMount() {
         this.getAllEvents()
-        this.getAllExpenses()
     }
 
     getAllEvents = async () => {
@@ -38,17 +32,7 @@ export default class Home extends Component {
         }
     }
 
-    getAllExpenses = async () => {
-        try {
-            const res = await axios.get('/api/event')
-            const newState = { ...this.state }
-            newState.allEvents = res.data
-            this.setState(newState)
-        } catch (err) {
-            console.log('failed to get all events')
-            console.log(err)
-        }
-    }
+   
 
     onDeleteEvent = async (eventId) => {
         try {
@@ -60,17 +44,26 @@ export default class Home extends Component {
         }
     }
 
+    onPaidOuting = async (eventId) => {
+        try{
+            await axios.put(`/api/event/${eventId}`, { isPaid: true })
+            console.log(eventId)
+                this.getAllEvents()
+        } catch (err) {
+            console.log('failed to update isPaid')
+            console.log(err)
+            }
+    }
+
     toggleAddEventField = () => {
         const showAddEventForm = !this.state.showAddEventForm;
         this.setState({ showAddEventForm })
     }
 
-    toggleExpenseField = () => {
-        const showExpenseField = !this.state.showExpenseField;
-        this.setState({ showExpenseField })
-    }
+
 
     render() {
+        const { allEvents,isPaid } = this.state
         console.log('all events:', this.state.allEvents)
         return (
             <div>
@@ -86,49 +79,35 @@ export default class Home extends Component {
                         getAllExpenses={ this.getAllExpenses }
                         getAllEvents={ this.getAllEvents } /> : null }
 
-                { this.state.allEvents.map((event) => {
+                { allEvents.map((event) => {
                     return (
+
                         <div className='allEvent-form'>
-                            <div> { event.name } </div>
-                            <div>{ event.category }</div>
+                            <Link to={ `/event/${event._id}` }>
+                                <div> { event.outing } </div>
+                            </Link>
                             <div>Date of Event:{ event.date }</div>
                             <div>Deadline: { event.deadline }</div>
                             <div>Budget:{ event.budget }</div>
                             <div>Notes:{ event.note }</div>
 
+                            { isPaid === true
+                                ? <button className="material-icons" onClick={ () => this.onPaidOuting(event._id) }> payment </button> : <button className="material-icons" > receipt </button>
+                                // fix this
+
+                            }
+
+
                             <button onClick={ () => this.onDeleteEvent(event._id) }>Delete</button>
 
-                            <button onClick={ () => this.toggleExpenseField }>Add Expense</button>
-
                         </div>
                     )
                 }) }
 
-                { this.state.allExpenses.map((expense) => {
-                    return (
-                        <div>
-                            <div>{ expense.category }</div>
-                            <div>{ expense.amount }</div>
-                            <div>{ expense.savedAmt }</div>
-                            <div>{ expense.note }</div>
-                        </div>
-                    )
-                }) }
+
 
                 {/* toggle create box: if no event created
                 show box but if an event is created box will disappear */}
-
-
-
-
-                {/* // { this.state.showExpenseField === true
-                //     ? <ExpenseForm
-                //         getAllEvents={ this.getAllEvents }
-                //         toggleExpenseField={ this.toggleExpenseField } /> : null } */}
-
-
-
-
 
             </div>
         )
