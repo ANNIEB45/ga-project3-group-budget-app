7848c1f1e6d6f95164347f95a632e5b18df8eca5
Anne Bellefleur <annebellefleur@Annes-MacBook-Air.local>
2020-05-27 03:16:00 -0400

    updated package.json for heroku

--- client/src/components/Home-Component/Home.js
+++ client/src/components/Home-Component/Home.js
@@ -91,6 +91,7 @@ export default class Home extends Component {
                 {/* BUTTON WORKS */ }
 
                 <LineChart />
+                
                 { this.state.showAddEventForm === true
                     ? <CreateForm
                         toggleAddEventField={ this.toggleAddEventField }
